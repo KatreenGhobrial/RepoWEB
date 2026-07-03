446d0bc5e38e6a1a4c392a8d19a0217045e3f23a
esterperetz <ester.peretz99@gmail.com>
2026-07-03 19:03:57 +0300

    Refactor(Frontend): Remove mentor endpoints from iotService

--- React_Iot_help_bot_layout/src/IoTManagement/iotService.js
+++ React_Iot_help_bot_layout/src/IoTManagement/iotService.js
@@ -1,45 +1,8 @@
 import api from '../apiClient';
 
-// Mentor Endpoints
-export async function getProjects() {
-  return await api.get("/mentor/projects");
-}
-
-export async function updateProjectPhase(projectId, phase) {
-  return await api.put(`/mentor/projects/${projectId}/phase`, { phase });
-}
-
-export async function getDashboard() {
-  return await api.get("/mentor/dashboard");
-}
-
-export async function getTasks(projectId) {
-  return await api.get(`/tasks/${projectId}`);
-}
-
-export async function giveFeedback(data) {
-  return await api.post("/mentor/feedback", data);
-}
-
-export async function getFeedback(projectId) {
-  return await api.get(`/mentor/feedback/${projectId}`);
-}
-
-export async function broadcast(message) {
-  return await api.post("/mentor/broadcast", { message });
-}
-
-export async function updateProjectEvaluation(projectId, evalData) {
-  return await api.put(`/mentor/projects/${projectId}/evaluation`, evalData);
-}
-
 // Library Endpoints
 export async function getAll() {
   return await api.get("/library");
-}
-
-export async function submitProjectAssessment(projectId, data) {
-  return await api.put(`/projects/${projectId}/assessment`, data);
 }
 
 export async function getBrokers() {
